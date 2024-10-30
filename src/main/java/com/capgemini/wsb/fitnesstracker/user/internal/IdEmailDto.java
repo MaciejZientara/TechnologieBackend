a5a1a752f8f921bbbde1cede2531a7392deeb8f2
Katarzyna Koloda <koloda.katarzyna@gmail.com>
2024-10-30 22:42:45 +0100

    fix typos

--- src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/IdEmailDto.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/IdEmailDto.java
@@ -2,6 +2,6 @@ package com.capgemini.wsb.fitnesstracker.user.internal;
 
 import jakarta.annotation.Nullable;
 
-public record IdEmailDto(@Nullable Long Id, String email) {
+public record IdEmailDto(@Nullable Long id, String email) {
 }
 
--- src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserSimpleDto.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserSimpleDto.java
@@ -2,5 +2,5 @@ package com.capgemini.wsb.fitnesstracker.user.internal;
 
 import jakarta.annotation.Nullable;
 
-public record UserSimpleDto(@Nullable Long Id, String firstName, String lastName) {
+public record UserSimpleDto(@Nullable Long id, String firstName, String lastName) {
 }
