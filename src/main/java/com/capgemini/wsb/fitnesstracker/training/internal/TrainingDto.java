eeed282dfc2f17a3ef689053b44dfbd3cf3b601c
MaciejZientara <mtzientara@gmail.com>
2024-11-14 20:51:40 +0100

    working training test 1

---
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingDto.java
@@ -0,0 +1,13 @@
+package com.capgemini.wsb.fitnesstracker.training.internal;
+
+import com.capgemini.wsb.fitnesstracker.user.api.User;
+
+import java.util.Date;
+
+public record TrainingDto(User user,
+                          Date startTime,
+                          Date endTime,
+                          ActivityType activityType,
+                          double distance,
+                          double averageSpeed) {
+}
---
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingMapper.java
@@ -0,0 +1,22 @@
+package com.capgemini.wsb.fitnesstracker.training.internal;
+
+import com.capgemini.wsb.fitnesstracker.training.api.Training;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class TrainingMapper {
+    TrainingDto toDto(Training training) {
+        return new TrainingDto(training.getUser(),
+                training.getStartTime(),
+                training.getEndTime(),
+                training.getActivityType(),
+                training.getDistance(),
+                training.getAverageSpeed()
+        );
+    }
+
+}
+
+
